10b0d92d082d5bb3fcc45ffc4fa7df30fe43a868
/ <96525548+PedroHenriqueW@users.noreply.github.com>
2023-05-10 15:30:37 -0300

    feat(logging): Implement robust logging system

--- Back-End/ToDoListBackEnd/Program.cs
+++ Back-End/ToDoListBackEnd/Program.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ToDoListBackEnd.Context;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DatabaseContext>(opt => opt.UseInMemoryDatabase("ToDoList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure Serilog to write to a log file
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.File("logs/log.txt")
+    .CreateLogger();
+
+// Configure the logging factory
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(dispose: true);
 
 var app = builder.Build();
 
