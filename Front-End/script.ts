9e6ff2bf99cf1ed82cb11ad7b5cae68c9ed3ce59
/ <96525548+PedroHenriqueW@users.noreply.github.com>
2023-05-18 18:08:52 -0300

    Convert JavaScript code to TypeScript.

---
+++ Front-End/script.ts
@@ -0,0 +1,86 @@
+const https = require("https");
+const axios = require("axios");
+
+// Create an instance of an HTTPS agent with verification disabled
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
+const api = axios.create({
+  baseURL: "https://localhost:7184/api",
+  httpsAgent,
+});
+
+interface TodoItem {
+  name: string;
+  isComplete: boolean;
+}
+
+async function getAllTodoItemsAsync() {
+  try {
+    const response = await api.get("/ToDoItems");
+    console.log(response);
+  } catch (error) {
+    console.error("Failed to get all todo items:", error);
+  }
+}
+
+async function getTodoItemByIdAsync(id: number) {
+  try {
+    const response = await api.get(`/ToDoItems/${id}`);
+    console.log(response);
+  } catch (error) {
+    console.error(`Failed to get todo item with id ${id}:`, error);
+  }
+}
+
+async function putTodoItemAsync(id: number, todoItem: TodoItem) {
+  try {
+    const response = await api.put(`/ToDoItems/${id}`, {
+      Id: id,
+      Name: todoItem.name,
+      IsComplete: todoItem.isComplete,
+    });
+    console.log(response);
+  } catch (error) {
+    console.error(`Failed to update todo item with id ${id}:`, error);
+  }
+}
+
+async function postTodoItemAsync(todoItem: TodoItem) {
+  try {
+    const response = await api.post("/ToDoItems", {
+      Name: todoItem.name,
+      IsComplete: todoItem.isComplete,
+    });
+    console.log(response);
+  } catch (error) {
+    console.error("Failed to create new todo item:", error);
+  }
+}
+
+async function deleteTodoItemByIdAsync(id: number) {
+  try {
+    const response = await api.delete(`/ToDoItems/${id}`);
+    console.log(response);
+  } catch (error) {
+    console.error(`Failed to delete todo item with id ${id}:`, error);
+  }
+}
+
+const todoItem: TodoItem = {
+  name: "Test name",
+  isComplete: false,
+};
+
+const updatedTodoItem: TodoItem = {
+  name: "Updated name",
+  isComplete: true,
+};
+
+// tsc \\
+getAllTodoItemsAsync();
+//getTodoItemByIdAsync(1);
+//putTodoItemAsync(1, updatedTodoItem);
+//postTodoItemAsync(todoItem);
+//deleteTodoItemByIdAsync(1);
